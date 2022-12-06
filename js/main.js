2a1df2fcdfd686f64af8c9c5c724ed0d5dcdf5cd
mintree08 <113869782+mintree08@users.noreply.github.com>
2022-12-07 05:17:55 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,16 +4,19 @@ let observer = new IntersectionObserver((e)=>{
     e.forEach((박스)=>{
         if (박스.isIntersecting){
             박스.target.style.opacity = 1;
-            박스.target.style.transform = 'translateY(0)';
+            박스.target.style.transform = 'translateY(0px)';
         }
         박스.intersectionRatio
     })
 })
 
 
-let content = document.querySelectorAll(".content", ".main", "#contact")
+let content = document.querySelectorAll(".content")
 observer.observe(content[0])
 observer.observe(content[1])
 observer.observe(content[2])
 observer.observe(content[3])
 observer.observe(content[4])
+observer.observe(content[5])
+
+
