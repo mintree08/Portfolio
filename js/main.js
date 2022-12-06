b08f0d77bb183e319583bebaf8fcdf088d29d73b
mintree08 <113869782+mintree08@users.noreply.github.com>
2022-12-06 09:19:13 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// main js
+
+let observer = new IntersectionObserver((e)=>{
+    e.forEach((박스)=>{
+        if (박스.isIntersecting){
+            박스.target.style.opacity = 1;
+            박스.target.style.transform = 'translateY(0)';
+        }
+        박스.intersectionRatio
+    })
+})
+
+
+let content = document.querySelectorAll(".content", ".main", "#contact")
+observer.observe(content[0])
+observer.observe(content[1])
+observer.observe(content[2])
+observer.observe(content[3])
+observer.observe(content[4])
